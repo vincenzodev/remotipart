528221dc35a9cc79f0e58c7c1e551ed194123e17
Vincenzo Farruggia <vincenzo@ideaful.it>
2017-01-14 13:28:31 +0100

    Fix wrapper response error: js request

--- lib/remotipart/render_overrides.rb
+++ lib/remotipart/render_overrides.rb
@@ -14,7 +14,7 @@ module Remotipart
 
     def render_with_remotipart *args
       render_without_remotipart(*args)
-      if remotipart_submitted?
+      if remotipart_submitted? and !request.xhr?
         textarea_body = response.content_type == 'text/html' ? html_escape(response.body) : response.body
         response.body = %{<script type=\"text/javascript\">try{window.parent.document;}catch(err){document.domain=document.domain;}</script> <textarea data-type=\"#{response.content_type}\" data-status=\"#{response.response_code}\" data-statusText=\"#{response.message}\">#{textarea_body}</textarea>}
         response.content_type = ::Rails.version >= '5' ? Mime[:html] : Mime::HTML
